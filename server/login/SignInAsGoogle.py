d6c8f410b5079d5790b115970a540911d67ede9c
yuvlian <138542238+yuvlian@users.noreply.github.com>
2024-11-09 16:31:23 +0700

    feat: support google login for mobile play

---
+++ server/login/SignInAsGoogle.py
@@ -0,0 +1,26 @@
+from limbus.requests import Cs, ReqSignInAsGoogle
+from limbus.responses import Sc, RspSignInAsGoogle
+from limbus.formats import UserAuthFormat, AccountInfoFormat
+from database.user import check_user
+from utils import get_date_time
+
+
+async def handle(req: Cs[ReqSignInAsGoogle]):
+    token = req.parameters.googleToken
+    user_id = check_user(token)
+    auth = UserAuthFormat(
+        last_update_date=get_date_time(),
+        last_login_date=get_date_time(),
+        uid=user_id,
+        public_id=user_id,
+        data_version=16,
+        db_id=0,
+        auth_code="furinalc",
+    )
+    rsp = RspSignInAsGoogle(
+        userAuth=auth,
+        accountInfo=AccountInfoFormat(uid=user_id),
+        walletCurrency="IDR",
+    )
+
+    return Sc[RspSignInAsGoogle](result=rsp)
